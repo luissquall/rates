829d015c74b0e39dbf4e4215458535336ae088f0
Luis Ramos <luissquall@gmail.com>
2018-06-28 13:07:04 -0500

    Release v1.0

---
+++ controllers/rates.js
@@ -0,0 +1,72 @@
+const rp = require('request-promise-native');
+const cheerio = require('cheerio');
+
+exports.convert = async function (request, h) {
+    let options = {
+        uri: 'http://www.eldolar.info/en/mexico/dia/hoy',
+        transform: (body) => {
+            return cheerio.load(body);
+        }
+    };
+
+    let response = await rp(options)
+        .then(($) => {
+            let rates = [];
+            let result = null;
+            let response = {};
+
+            $('#dllsTable tbody > tr').each((i, el) => {
+                let $row = $(el);
+                let entity = $row.find('td').first().find('.small-hide').text();
+                let buy = $($row.find('td.xTimes')[0]).text();
+                let sell = $($row.find('td.xTimes')[1]).text();;
+
+                if (!sell) {
+                    sell = buy;
+                }
+
+                rates.push({ entity, buy, sell });
+            });
+
+            for (let rate of rates) {
+                if (rate.entity.match(/Banamex/i)) {
+                    result = rate;
+                    break;
+                }
+            }
+
+            if (result) {
+                response = {
+                    data: {
+                        from: 'USD',
+                        to: 'MXN',
+                        rate: result.sell,
+                        entity: result.entity,
+                        source: {
+                            name: 'ElDolar.Info',
+                            url: options.uri
+                        }
+                    }
+                };
+            } else {
+                response = {
+                    error: {
+                        code: 500,
+                        message: 'Banamex entity was not found'
+                    }
+                }
+            }
+
+            return response;
+        })
+        .catch((err) => {
+            return {
+                error: {
+                    code: 500,
+                    message: err.toString()
+                }
+            };
+        });
+
+    return response;
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Import modules
+require('dotenv').config();
+
+const Hapi =  require('hapi');
+
+// Controllers
+const Rates = require('./controllers/rates.js');
+
+// Create a server with a host and port
+const server = Hapi.server({
+    port: process.env.PORT
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/usd/mxn',
+    handler: Rates.convert
+});
+
+// Start the server
+async function start() {
+    try {
+        await server.start();
+    } catch (err) {
+        console.log(err);
+
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
