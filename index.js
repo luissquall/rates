cf181fa04685f7bfa4936a9e3cf0b60581751eea
Luis Ramos <luissquall@gmail.com>
2021-09-13 07:24:34 -0500

    Refactor create server code

--- index.js
+++ index.js
@@ -8,34 +8,32 @@ const Hapi = require('@hapi/hapi');
 // Controllers
 const Rates = require('./controllers/rates.js');
 
-// Create a server with a host and port
-const server = Hapi.server({
-    port: process.env.PORT
-});
+// Init server
+const init = async () => {
+	const server = Hapi.server({
+		port: process.env.PORT
+	});
 
-// Add the routes
-server.route({
-	method: 'GET',
-	path: '/usd/mxn/{entity?}',
-	handler: Rates.convert
-});
-server.route({
-	method: 'GET',
-	path: '/eur/mxn/{entity?}',
-	handler: Rates.convert
-});
+	// Add routes
+	server.route({
+		method: 'GET',
+		path: '/usd/mxn/{entity?}',
+		handler: Rates.convert
+	});
+	server.route({
+		method: 'GET',
+		path: '/eur/mxn/{entity?}',
+		handler: Rates.convert
+	});
 
-// Start the server
-async function start() {
-    try {
-        await server.start();
-    } catch (err) {
-        console.log(err);
+	await server.start();
 
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
+	console.log('Server running on %s', server.info.uri);
 };
 
-start();
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
